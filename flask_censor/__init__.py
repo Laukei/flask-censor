7f07c94b393ba5a5f772005c56a4833c973f5414
Rob Heath <rob@robheath.me.uk>
2019-05-21 14:45:41 +0100

    changed filename to allow import; created MANIFEST.in; added __version__ to __init__

--- flask_censor/__init__.py
+++ flask_censor/__init__.py
@@ -1,4 +1,6 @@
 # based loosely on Ben Friedland's Profanity module https://github.com/ben174/profanity
 # and on Mardix's flask-recaptcha module https://github.com/mardix/flask-recaptch
 
+__version__ = 0.1
+
 from .logic import Censor
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import setuptools
+import flask_censor
 
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
 	name = "flask-censor",
-	version = "0.1",
+	version = flask_censor.__version__,
 	author = "Robert Heath",
 	author_email = "rob@robheath.me.uk",
-	description = "A profanity censor with hooks for Flask and Jinja",
+	description = "A profanity censor with hooks for Flask and Jinja2",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	url = "https://github.com/Laukei/flask-censor",
-	packages = setuptools.find_packages(),
+	packages = ['flask_censor'],
 	classifiers = [
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 		"Development Status :: 3 - Alpha"]
-
 	)
